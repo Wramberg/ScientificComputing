94a715bd837c31dab3d83fd640c718f67f03520e
Jesper Wramberg <jesper.wramberg@gmail.com>
2014-04-14 18:31:45 +0200

    stuffsz

--- code/sim.py
+++ code/sim.py
@@ -4,14 +4,17 @@ import scipy.signal as signal
 import matplotlib.pyplot as plt
 import sys
 sys.path.append('/home/tausen/p8/git/code/')
-import basics
+#import basics
 
+params = {'legend.fontsize': 15,
+          'legend.linewidth': 2}
+plt.rcParams.update(params)
 
-N = int(3e3)
+N = int(30e3)
 fif = 800e3
 f0 = 1.9e6
 kco = 500e3
-fs = 0
+#fs = 0
 
 
 def getinput():
@@ -23,7 +26,6 @@ def getinput():
     fsymb = np.genfromtxt("gmsk_fsymb.dat")
 
     fs = fsymb * os
-
     tn = np.arange(len(I)) / fs
     x = I * np.cos(2 * np.pi * fif * tn) - Q * np.sin(2 * np.pi * fif * tn)
 
@@ -34,7 +36,6 @@ def getinput():
 
 def gety(tn, phiy):
     global f0
-
     y = np.cos(2 * np.pi * f0 * tn + phiy)
     return y
 
@@ -71,7 +72,90 @@ def loop(x, tn):
 
     return u, v, phiy, s
 
-if fs == 0:
-    tn, x, phix = getinput()
 
+def plotphases(phix, phiy, phiycomp=None):
+    plt.figure(figsize=(11,2.75), dpi=200)   
+    plt.plot(phix, label='Phase of input signal')
+    plt.plot(phiy, label='Phase of output signal')
+    if phiycomp is not None:
+        plt.plot(phiycomp, label='Compensated phase of output signal')
+    plt.grid()
+    plt.xlabel('Samples')
+#    plt.ylim(-10, 12)
+    plt.legend(loc=3, prop={'size': 12}, labelspacing=0.25)
+    plt.subplots_adjust(left=0.1, right=0.9, bottom=0.175)
+    if phiycomp is not None:
+        plt.savefig('phasesignalscomp.eps') 
+    else:
+        plt.savefig('phasesignals.eps') 
+
+
+def getphaseerror(phix, phiy):
+    try:
+        diff = phix[:phiy.size] - phiy
+    except:
+        raise Exception('The length of the phiy signal must be smaller than the length of the phix signal')
+    err = np.sqrt(np.mean(diff[250:] ** 2))
+    return diff, err
+
+
+def phasecomp(phix, phiy):
+    # Find phase offset
+    diff = phix[250:] - phiy[250:]
+    poff = np.mean(diff)
+
+    # Find time offset
+    maxoffset = 50
+    rmserr = np.empty(maxoffset)
+    for i in np.arange(0, maxoffset):
+        dummy, rmserr[i] = getphaseerror(phix[200:], phiy[200+maxoffset-i:]+poff)
+
+    toff = maxoffset - np.argmin(rmserr)
+    if toff < 0:
+        raise Exception('Got negative time offset, time delay should be positive')
+
+    return phiy[toff:] + poff
+
+
+def getperiodogram(x):
+    global fs
+    freqs, psd = signal.welch(x, fs=fs, window='boxcar', nperseg=300, noverlap=None, detrend='constant', return_onesided=True)
+    return freqs, psd
+
+
+def plotpsd(x, y):
+    freqsx, psdx = getperiodogram(x)
+    freqsy, psdy = getperiodogram(y)
+    plt.figure(figsize=(11,2.75), dpi=200)  
+    plt.plot(freqsx/1e3, psdx, label='PSD of input signal')
+    plt.plot(freqsy/1e3, psdy, label='PSD of output signal')
+    plt.grid()
+    plt.xlim(0, 3.5e3)
+    plt.xlabel('Frequency [kHz]')
+    plt.legend(loc=1, prop={'size': 12}, labelspacing=0.25)
+    plt.subplots_adjust(left=0.1, right=0.9, bottom=0.175)
+    plt.savefig('psds.eps') 
+
+
+def plotphaseerr(x):
+    plt.figure(figsize=(11,2.75), dpi=200)  
+    plt.plot(x)
+    plt.grid()
+    plt.xlabel('Samples')
+    plt.subplots_adjust(left=0.1, right=0.9, bottom=0.175)
+    plt.savefig('phaseerr.eps')  
+  
+
+tn, x, phix = getinput()
 u, v, phiy, s = loop(x, tn)
+y = gety(tn, phiy)
+phiycomp = phasecomp(phix, phiy)
+
+plotphases(phix, phiy, phiycomp)
+plotpsd(x, y)
+
+diff, rms = getphaseerror(phix, phiycomp)
+plotphaseerr(diff)
+print rms
+print 180/np.pi * rms
+#plt.show()
