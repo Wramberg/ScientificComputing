18df21e5b63277af550c8781e444662b8c16333d
Mathias Tausen <mathias.tausen@gmail.com>
2014-04-16 10:56:59 +0200

    Few adjustments to code snippets and code, scaled y-axis on psds

--- code/sim.py
+++ code/sim.py
@@ -2,8 +2,7 @@ from __future__ import division
 import numpy as np
 import scipy.signal as signal
 import matplotlib.pyplot as plt
-import sys
-sys.path.append('/home/tausen/p8/git/code/')
+
 
 params = {'legend.fontsize': 15,
           'legend.linewidth': 2}
@@ -19,12 +18,16 @@ fs = 0
 def getinput():
     global N, fif, fs
 
+    # Load first N samples of I and Q from text files
     I = np.genfromtxt("gmsk_I.dat")[:N]
     Q = np.genfromtxt("gmsk_Q.dat")[:N]
-    os = np.genfromtxt("gmsk_os.dat")
-    fsymb = np.genfromtxt("gmsk_fsymb.dat")
 
-    fs = fsymb * os
+    # Get oversampling ratio and symbol rate, calculate sample frequency
+    os    = np.genfromtxt("gmsk_os.dat")
+    fsymb = np.genfromtxt("gmsk_fsymb.dat")
+    fs    = fsymb * os
+
+    # Generate time axis and input signal x
     tn = np.arange(len(I)) / fs
     x = I * np.cos(2 * np.pi * fif * tn) - Q * np.sin(2 * np.pi * fif * tn)
 
@@ -56,24 +59,28 @@ def loop(x, tn):
     v    = np.empty(N)
     phiy = np.empty(N)
 
+    # Running sum of v up to n
+    vsum = vm1
+
     # Calculate signals for n = 0
     u[0]    = x[0] * sm1
     v[0]    = u[0] * b[0] + um1 * b[1] - vm1 * a[1]
-    phiy[0] = 2 * np.pi * kco * 1 / fs * np.sum(v[:1])
+    phiy[0] = 2 * np.pi * kco * 1 / fs * vsum
     s[0]    = np.cos(2 * np.pi * fif * tn[0] + phiy[0])
 
     # Main loop
-    for n in range(1, N):
+    for n in np.arange(1, N):
         u[n]    = x[n] * s[n - 1]
         v[n]    = u[n] * b[0] + u[n - 1] * b[1] - v[n - 1] * a[1]
-        phiy[n] = 2 * np.pi * kco * 1 / fs * np.sum(v[:n + 1])
+        phiy[n] = 2 * np.pi * kco * 1 / fs * vsum
         s[n]    = np.cos(2 * np.pi * fif * tn[n] + phiy[n])
+        vsum   += v[n]
 
     return u, v, phiy, s
 
 
 def plotphases(phix, phiy, phiycomp=None):
-    plt.figure(figsize=(11,2.75), dpi=200)   
+    plt.figure(figsize=(11, 2.75), dpi=200)
     plt.plot(phix, label='Phase of input signal')
     plt.plot(phiy, label='Phase of output signal')
     if phiycomp is not None:
@@ -84,9 +91,9 @@ def plotphases(phix, phiy, phiycomp=None):
     plt.legend(loc=3, prop={'size': 12}, labelspacing=0.25)
     plt.subplots_adjust(left=0.1, right=0.9, bottom=0.175)
     if phiycomp is not None:
-        plt.savefig('phasesignalscomp.eps') 
+        plt.savefig('phasesignalscomp.eps')
     else:
-        plt.savefig('phasesignals.eps') 
+        plt.savefig('phasesignals.eps')
 
 
 def getphaseerror(phix, phiy):
@@ -107,7 +114,7 @@ def phasecomp(phix, phiy):
     maxoffset = 50
     rmserr = np.empty(maxoffset)
     for i in np.arange(0, maxoffset):
-        dummy, rmserr[i] = getphaseerror(phix[200:], phiy[200+maxoffset-i:]+poff)
+        dummy, rmserr[i] = getphaseerror(phix[200:], phiy[200 + maxoffset - i:] + poff)
 
     toff = maxoffset - np.argmin(rmserr)
     if toff < 0:
@@ -124,25 +131,25 @@ def getperiodogram(x):
 def plotpsd(x, y):
     freqsx, psdx = getperiodogram(x)
     freqsy, psdy = getperiodogram(y)
-    plt.figure(figsize=(11,2.75), dpi=200)  
-    plt.plot(freqsx/1e3, psdx, label='PSD of input signal')
-    plt.plot(freqsy/1e3, psdy, label='PSD of output signal')
+    plt.figure(figsize=(11, 2.75), dpi=200)
+    plt.plot(freqsx / 1e3, psdx * 1e6, label='PSD of input signal $\cdot 10^6$')
+    plt.plot(freqsy / 1e3, psdy * 1e6, label='PSD of output signal $\cdot 10^6$')
     plt.grid()
     plt.xlim(0, 3.5e3)
     plt.xlabel('Frequency [kHz]')
     plt.legend(loc=1, prop={'size': 12}, labelspacing=0.25)
     plt.subplots_adjust(left=0.1, right=0.9, bottom=0.175)
-    plt.savefig('psds.eps') 
+    plt.savefig('psds.eps')
 
 
 def plotphaseerr(x):
-    plt.figure(figsize=(11,2.75), dpi=200)  
+    plt.figure(figsize=(11, 2.75), dpi=200)
     plt.plot(x)
     plt.grid()
     plt.xlabel('Samples')
     plt.subplots_adjust(left=0.1, right=0.9, bottom=0.175)
-    plt.savefig('phaseerr.eps')  
-  
+    plt.savefig('phaseerr.eps')
+
 
 tn, x, phix = getinput()
 u, v, phiy, s = loop(x, tn)
@@ -154,15 +161,3 @@ plotpsd(x, y)
 
 diff, rms = getphaseerror(phix, phiycomp)
 plotphaseerr(diff)
-
-#outfreq = f0 + kco * v
-#plt.figure()
-#plt.figure(figsize=(11,2.75), dpi=200)  
-#plt.plot(outfreq/1e6)
-#plt.grid()
-#plt.ylabel('Frequency [MHz]')
-#plt.xlabel('Samples')
-#plt.subplots_adjust(left=0.1, right=0.9, bottom=0.175)
-#plt.savefig('outfreq.eps')  
-
-#plt.show()
